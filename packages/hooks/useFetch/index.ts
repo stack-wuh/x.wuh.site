6cd2b69ffe3dfde5183b93fd1986aff762e1c075
吴尒红 <724132485@qq.com>
2026-06-07 19:24:50 +0800

    fix: 删除 index.ts 中对已移除的 useFetch 模块的重导出

--- packages/hooks/useFetch/index.ts
+++ packages/hooks/useFetch/index.ts
@@ -1,4 +1,2 @@
 export { fetcher } from './fetcher'
 export type { FetchError, FetchResult, QueryRecord, QueryValue, RequestOptions } from './fetcher'
-export { default as useFetch } from './useFetch'
-export type { UseFetchOptions, UseFetchReturn } from './useFetch'
